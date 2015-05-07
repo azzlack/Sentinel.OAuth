ecc0c699e8b9b218531ef658b6a85e571afc359d
Ove Andersen <ove.andersen@outlook.com>
2015-05-07 19:20:46 +0200

    Started building an asp.net identity compatible user manager

--- src/AspNetIdentityUserManager/Implementation/AspNetIdentityUserManager.cs
+++ src/AspNetIdentityUserManager/Implementation/AspNetIdentityUserManager.cs
@@ -1,9 +1,33 @@
 ﻿namespace Sentinel.OAuth.UserManagers.AspNetIdentityUserManager.Implementation
 {
-    using Sentinel.OAuth.Core.Interfaces.Managers;
+    using System.Threading.Tasks;
 
-    public class AspNetIdentityUserManager : IUserManager
+    using Microsoft.AspNet.Identity;
+
+    using Sentinel.OAuth.Core.Interfaces.Identity;
+    using Sentinel.OAuth.Core.Interfaces.Managers;
+    using Sentinel.OAuth.UserManagers.AspNetIdentityUserManager.Models;
+
+    public class AspNetIdentityUserManager : UserManager<User>, IUserManager
     {
-         
+        /// <summary>
+        /// Initializes a new instance of the AspNetIdentityUserManager
+        /// class.
+        /// </summary>
+        /// <param name="store">The store.</param>
+        public AspNetIdentityUserManager(IUserStore<User> store)
+            : base(store)
+        {
+        }
+
+        public Task<ISentinelPrincipal> AuthenticateUserWithPasswordAsync(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ISentinelPrincipal> AuthenticateUserAsync(string username)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ src/AspNetIdentityUserManager/SentinelContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Sentinel.OAuth.UserManagers.AspNetIdentityUserManager
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Sentinel.OAuth.UserManagers.AspNetIdentityUserManager.Models;
+
+    public class SentinelContext : IdentityDbContext<User>
+    {
+        public SentinelContext()
+            : base("DefaultConnection", false)
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static SentinelContext Create()
+        {
+            return new SentinelContext();
+        }
+    }
+}
