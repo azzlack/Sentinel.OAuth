2467f71711aeae5b775802d71d9a460dc331d898
Ove Andersen <ove.andersen@outlook.com>
2015-05-07 19:00:35 +0200

    Set correct assembly name

--- src/Sentinel.OAuth.TokenManagers.RavenDbTokenRepository/Properties/AssemblyInfo.cs
+++ src/Sentinel.OAuth.TokenManagers.RavenDbTokenRepository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RavenDbTokenRepository")]
+[assembly: AssemblyTitle("Sentinel.OAuth.TokenManagers.RavenDbTokenRepository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RavenDbTokenRepository")]
+[assembly: AssemblyProduct("Sentinel.OAuth.TokenManagers.RavenDbTokenRepository")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/Sentinel.OAuth.UserManagers.SqlServerUserManager/Properties/AssemblyInfo.cs
+++ src/Sentinel.OAuth.UserManagers.SqlServerUserManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlServerUserManager")]
+[assembly: AssemblyTitle("Sentinel.OAuth.UserManagers.SqlServerUserManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlServerUserManager")]
+[assembly: AssemblyProduct("Sentinel.OAuth.UserManagers.SqlServerUserManager")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
