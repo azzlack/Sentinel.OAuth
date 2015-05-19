63dc1ebff4557833883cc930c91b09a0afc1ce0e
Ove Andersen <ove.andersen@outlook.com>
2015-05-19 15:38:51 +0200

    Added missing Created property for Redis refresh tokens

--- src/Sentinel.OAuth.TokenManagers.RedisTokenRepository/Implementation/RedisTokenFactory.cs
+++ src/Sentinel.OAuth.TokenManagers.RedisTokenRepository/Implementation/RedisTokenFactory.cs
@@ -56,6 +56,7 @@
                            Subject = userId,
                            Token = token,
                            ValidTo = validTo
+                           Created = DateTime.UtcNow
                        };
         }
 
