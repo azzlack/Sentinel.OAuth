b8cb96909e41bf4b4054741b2eec1aac09fdd3c7
Ove Andersen <ove.andersen@outlook.com>
2015-05-29 00:54:49 +0200

    Added shortcut methods to sentinelprincipal, for easier access to common claims

--- src/Sentinel.OAuth.Core/Interfaces/Identity/ISentinelPrincipal.cs
+++ src/Sentinel.OAuth.Core/Interfaces/Identity/ISentinelPrincipal.cs
@@ -1,5 +1,7 @@
 ﻿namespace Sentinel.OAuth.Core.Interfaces.Identity
 {
+    using System;
+    using System.Collections.Generic;
     using System.Security.Principal;
 
     /// <summary>Defines the basic functionality of a Principal object.</summary>
@@ -8,5 +10,21 @@
         /// <summary>Gets the Sentinel identity.</summary>
         /// <value>The Sentinel identity.</value>
         new ISentinelIdentity Identity { get; }
+
+        /// <summary>Gets the time in UTC format when the identity expires.</summary>
+        /// <value>The expire time in UTC format.</value>
+        DateTime ValidTo { get; }
+
+        /// <summary>Gets the roles.</summary>
+        /// <value>The roles.</value>
+        IEnumerable<string> Roles { get; }
+
+        /// <summary>Gets the access token.</summary>
+        /// <value>The access token.</value>
+        string AccessToken { get; }
+
+        /// <summary>Gets the refresh token.</summary>
+        /// <value>The refresh token.</value>
+        string RefreshToken { get; }
     }
 }
--- src/Sentinel.OAuth/Models/Identity/SentinelPrincipal.cs
+++ src/Sentinel.OAuth/Models/Identity/SentinelPrincipal.cs
@@ -1,10 +1,13 @@
 ﻿namespace Sentinel.OAuth.Models.Identity
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Globalization;
     using System.Linq;
     using System.Security.Claims;
     using System.Security.Principal;
+    using System.Threading;
 
     using Sentinel.OAuth.Core.Constants.Identity;
     using Sentinel.OAuth.Core.Interfaces.Identity;
@@ -14,8 +17,7 @@
     public class SentinelPrincipal : ISentinelPrincipal
     {
         /// <summary>
-        ///     Initializes a new instance of the Sentinel.OAuth.Core.Models.Identity.SentinelPrincipal
-        ///     class.
+        /// Initializes a new instance of the Sentinel.OAuth.Models.Identity.SentinelPrincipal class.
         /// </summary>
         public SentinelPrincipal()
         {
@@ -23,11 +25,10 @@
         }
 
         /// <summary>
-        ///     Initializes a new instance of the Sentinel.OAuth.Core.Models.Identity.JsonPrincipal
-        ///     class.
+        /// Initializes a new instance of the Sentinel.OAuth.Models.Identity.SentinelPrincipal class.
         /// </summary>
-        /// <exception cref="System.ArgumentNullException">
-        ///     Thrown when one or more required arguments are null.
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when one or more required arguments are null.
         /// </exception>
         /// <param name="identity">The identity.</param>
         public SentinelPrincipal(ISentinelIdentity identity)
@@ -66,22 +67,6 @@
             this.Identity = new SentinelIdentity(identity);
         }
 
-        /// <summary>Gets the identity.</summary>
-        /// <value>The identity.</value>
-        IIdentity IPrincipal.Identity
-        {
-            get
-            {
-                return this.Identity;
-            }
-        }
-
-        /// <summary>
-        /// Gets the actual identity.
-        /// </summary>
-        /// <value>The actual identity.</value>
-        public ISentinelIdentity Identity { get; private set; }
-
         /// <summary>Gets an unauthorized/anonymous Sentinel principal object.</summary>
         /// <value>An unauthorized/anonymous Sentinel principal object.</value>
         public static ISentinelPrincipal Anonymous
@@ -98,9 +83,83 @@
         {
             get
             {
-                return new SentinelPrincipal(ClaimsPrincipal.Current);
+                return new SentinelPrincipal(Thread.CurrentPrincipal);
             }
         }
+
+        /// <summary>Gets the identity.</summary>
+        /// <value>The identity.</value>
+        IIdentity IPrincipal.Identity
+        {
+            get
+            {
+                return this.Identity ?? SentinelIdentity.Anonymous;
+            }
+        }
+
+        /// <summary>Gets the time in UTC format when the identity expires.</summary>
+        /// <value>The expire time in UTC format.</value>
+        public DateTime ValidTo
+        {
+            get
+            {
+                var expireClaim = this.Identity.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Expiration);
+
+                DateTime expires;
+                if (expireClaim != null && DateTime.TryParseExact(expireClaim.Value, "O", CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out expires))
+                {
+                    return expires;
+                }
+
+                return DateTime.MinValue;
+            }
+        }
+
+        /// <summary>Gets the roles.</summary>
+        /// <value>The roles.</value>
+        public IEnumerable<string> Roles
+        {
+            get
+            {
+                return this.Identity.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value);
+            }
+        }
+
+        /// <summary>Gets the access token.</summary>
+        /// <value>The access token.</value>
+        public string AccessToken
+        {
+            get
+            {
+                if (this.Identity.HasClaim(x => x.Type == ClaimType.AccessToken))
+                {
+                    return this.Identity.Claims.First(x => x.Type == ClaimType.AccessToken).Value;
+                }
+
+                return string.Empty;
+            }
+        }
+
+        /// <summary>Gets the refresh token.</summary>
+        /// <value>The refresh token.</value>
+        public string RefreshToken
+        {
+            get
+            {
+                if (this.Identity.HasClaim(x => x.Type == ClaimType.RefreshToken))
+                {
+                    return this.Identity.Claims.First(x => x.Type == ClaimType.RefreshToken).Value;
+                }
+
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets the actual identity.
+        /// </summary>
+        /// <value>The actual identity.</value>
+        public ISentinelIdentity Identity { get; private set; }
 
         /// <summary>Determines whether the current principal belongs to the specified role.</summary>
         /// <param name="role">The name of the role for which to check membership.</param>
