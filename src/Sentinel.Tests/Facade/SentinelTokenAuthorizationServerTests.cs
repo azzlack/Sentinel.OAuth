1d944ee66052fba9a0dd67dfba07dd01ec0cf81c
Ove Andersen <ove.andersen@outlook.com>
2015-10-21 14:32:47 +0200

    Fixed failing tests

--- src/Sentinel.Tests/Facade/SentinelTokenAuthorizationServerTests.cs
+++ src/Sentinel.Tests/Facade/SentinelTokenAuthorizationServerTests.cs
@@ -1,11 +1,15 @@
 ﻿namespace Sentinel.Tests.Facade
 {
+    using Common.Logging;
     using Microsoft.Owin.Security.OAuth;
     using Microsoft.Owin.Testing;
     using NUnit.Framework;
     using Owin;
     using Sentinel.OAuth.Core.Models;
     using Sentinel.OAuth.Extensions;
+    using Sentinel.OAuth.Implementation.Managers;
+    using Sentinel.OAuth.Implementation.Providers;
+    using Sentinel.OAuth.Implementation.Repositories;
     using Sentinel.Sample.Managers;
     using System.Web.Http;
 
@@ -18,25 +22,33 @@
         {
             this.Server = TestServer.Create(
                 app =>
-                {
-                    // The easiest way to use Sentinel
-                    app.UseSentinelAuthorizationServer(new SentinelAuthorizationServerOptions()
                     {
-                        ClientManager = new SimpleClientManager(),
-                        UserManager = new SimpleUserManager()
+                        var cryptoProvider = new SHA2CryptoProvider();
+                        var principalProvider = new PrincipalProvider(cryptoProvider);
+                        var tokenRepository = new MemoryTokenRepository();
+                        var tokenProvider = new SentinelTokenProvider(cryptoProvider, principalProvider, tokenRepository);
+                        var userManager = new SimpleUserManager();
+
+                        app.UseSentinelAuthorizationServer(
+                            new SentinelAuthorizationServerOptions()
+                            {
+                                ClientManager = new SimpleClientManager(),
+                                UserManager = userManager,
+                                TokenProvider = tokenProvider,
+                                TokenManager = new TokenManager(LogManager.GetLogger<SentinelTokenAuthorizationServerTests>(), userManager, principalProvider, tokenProvider, tokenRepository)
+                            });
+
+                        // Start up web api
+                        var httpConfig = new HttpConfiguration();
+                        httpConfig.MapHttpAttributeRoutes();
+
+                        // Configure Web API to use only Bearer token authentication.
+                        httpConfig.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+                        httpConfig.EnsureInitialized();
+
+                        app.UseWebApi(httpConfig);
                     });
-
-                    // Start up web api
-                    var httpConfig = new HttpConfiguration();
-                    httpConfig.MapHttpAttributeRoutes();
-
-                    // Configure Web API to use only Bearer token authentication.
-                    httpConfig.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
-                    httpConfig.EnsureInitialized();
-
-                    app.UseWebApi(httpConfig);
-                });
 
             base.TestFixtureSetUp();
         }
