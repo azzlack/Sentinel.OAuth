282590715e1e2ec083ca0651ab5500bbe6bac662
Ove Andersen <ove.andersen@outlook.com>
2015-10-26 09:13:28 +0100

    Started adding support for decoding and validating token

---
+++ src/Sentinel.OAuth.Client/Helpers/JwtHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Sentinel.OAuth.Client.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class JwtHelper
+    {
+        public static IEnumerable<KeyValuePair<string, string>> DecodeHeader(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> DecodePayload(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool Verify(string accessToken, string idToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
