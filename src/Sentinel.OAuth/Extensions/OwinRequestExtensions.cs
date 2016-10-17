76c51e1c88a33d7964ccbb5c165e545e5119c80f
Ove Andersen <ove.andersen@outlook.com>
2016-10-17 11:05:04 +0200

    Moved owin request extension to main assembly

--- src/Sentinel.OAuth/Extensions/OwinRequestExtensions.cs
+++ src/Sentinel.OAuth/Extensions/OwinRequestExtensions.cs
@@ -1,12 +1,50 @@
-﻿namespace Sentinel.OAuth.Client.Mvc5.Extensions
+﻿namespace Sentinel.OAuth.Extensions
 {
     using System;
-    using System.Runtime.CompilerServices;
 
     using Microsoft.Owin;
 
     public static class OwinRequestExtensions
     {
+        /// <summary>Creates an absolute url from the specified path.</summary>
+        /// <param name="request">The request to act on.</param>
+        /// <param name="path">The relative path.</param>
+        /// <returns>The new absolute URL.</returns>
+        public static Uri CreateAbsoluteUrl(this IOwinRequest request, string path)
+        {
+            if (string.IsNullOrEmpty(path) || !request.IsRelativeUrl(path))
+            {
+                throw new ArgumentException(nameof(path));
+            }
+
+            return new Uri($"{request.Uri.Scheme}://{request.Uri.Host}{path}");
+        }
+
+        /// <summary>Checks if the specified url is a relative url.</summary>
+        /// <param name="request">The request to act on.</param>
+        /// <param name="url">The url to check.</param>
+        /// <returns>true if the url is relative, false if not.</returns>
+        public static bool IsRelativeUrl(this IOwinRequest request, string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return false;
+            }
+
+            // Check if it is a valid relative url
+            if (url[0] == 47 && (url.Length == 1 || url[1] != 47 && url[1] != 92))
+            {
+                return true;
+            }
+
+            if (url.Length > 1 && url[0] == 126)
+            {
+                return url[1] == 47;
+            }
+
+            return false;
+        }
+
         /// <summary>Compares the specified url with the request and determines if the url is on the same domain.</summary>
         /// <param name="request">The request to act on.</param>
         /// <param name="url">The url to compare.</param>
@@ -32,17 +70,7 @@
             }
 
             // Check if it is a valid relative url
-            if (url[0] == 47 && (url.Length == 1 || url[1] != 47 && url[1] != 92))
-            {
-                return true;
-            }
-
-            if (url.Length > 1 && url[0] == 126)
-            {
-                return url[1] == 47;
-            }
-
-            return false;
+            return request.IsRelativeUrl(url);
         }
 
         /// <summary>Compares the specified url with the request and determines if it is the same.</summary>
